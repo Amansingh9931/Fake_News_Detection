258d20a15a816dfb4ec5a5c380874f9db8e1685a
Aman Kumar <amansingh12073@gmail.com>
2026-03-11 20:50:13 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
 import NewsForm from "./component/News_Form";
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <NewsForm />
+      <Footer />
     </div>
   );
 }
